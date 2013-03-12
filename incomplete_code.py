7733572d6a971cc7c91ee201b12541c8ad8e8fe1
MobitZ <momo.fox@hotmail.com>
2013-03-12 19:25:00 +0700

    แก้แล้ว อิอิกำ

--- incomplete_code.py
+++ incomplete_code.py
@@ -41,7 +41,7 @@ class Taxi(Transportation):
    
 # main program
 travel_cost = 0
-
+#trip walk,taxi,train,taxi
 trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
          Taxi("KMITL SCB Bank","Ladkrabang Station",5),
          Train("Ladkrabang Station","Payathai Station",40,6),
